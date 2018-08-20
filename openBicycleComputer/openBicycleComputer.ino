3d887e15de71ee0695a76e66c4999d8d4072b3a9
Conor <conorf50@gmail@.com>
2018-08-20 17:42:40 +0100

    Removed references to SPI display, speed output is now over serial

--- openBicycleComputer/openBicycleComputer.ino
+++ openBicycleComputer/openBicycleComputer.ino
@@ -1,36 +1,10 @@
 /*********************************************************************
-This is an example for our Monochrome OLEDs based on SSD1306 drivers
 
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/category/63_98
+Bike computer
 
-This example is for a 128x64 size display using I2C to communicate
-3 pins are required to interface (2 I2C and one reset)
-
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
-products from Adafruit!
-
-Written by Limor Fried/Ladyada  for Adafruit Industries.  
-BSD license, check license.txt for more information
-All text above, and the splash screen must be included in any redistribution
+Based on original code by Dave Darko: https://github.com/davedarko/OpenBikeComputer
 *********************************************************************/
 
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-
-#define OLED_RESET 4
-Adafruit_SSD1306 display(OLED_RESET);
-
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-
-#if (SSD1306_LCDHEIGHT != 64)
-#error("Height incorrect, please fix Adafruit_SSD1306.h!");
-#endif
 
 
 unsigned long wheel = 2240; 
@@ -44,15 +18,10 @@ int is_pushed_old;
 
 void setup(void) {
   Serial.begin(9600);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
     pinMode(pushButton, INPUT_PULLUP);
-  display.display();
-  delay(1000);
-    display.clearDisplay();
-  display.display();
+
   constant = get_constant();
   is_released = LOW;
-    //Keyboard.begin();
 }
 
   double abc = 0;
@@ -73,16 +42,7 @@ void loop() {
   else {
     if (is_released == HIGH) {
       is_released = LOW;
-      /*
-      Keyboard.press(KEY_BACKSPACE);Keyboard.release(KEY_BACKSPACE);
-      Keyboard.press(KEY_BACKSPACE);Keyboard.release(KEY_BACKSPACE);
-      Keyboard.press(KEY_BACKSPACE);Keyboard.release(KEY_BACKSPACE);
-      Keyboard.press(KEY_BACKSPACE);Keyboard.release(KEY_BACKSPACE);
-      if (abc>=10) {Keyboard.press(KEY_BACKSPACE);Keyboard.release(KEY_BACKSPACE);}
-      if (abc>=100) {Keyboard.press(KEY_BACKSPACE);Keyboard.release(KEY_BACKSPACE);}
-      if (abc>=1000) {Keyboard.press(KEY_BACKSPACE);Keyboard.release(KEY_BACKSPACE);}
-      if (abc>=10000) {Keyboard.press(KEY_BACKSPACE);Keyboard.release(KEY_BACKSPACE);}
-      */
+
       time = micros() - start;
       abc = get_mps(time);
 //     String output_string;
@@ -92,12 +52,7 @@ void loop() {
       Serial.println(abc);
 //      Keyboard.print(abc);
 
-  display.clearDisplay();
-        display.setTextSize(4);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.println(abc);
-    display.display();
+;
 
       delay(10);
       
@@ -114,3 +69,4 @@ unsigned long get_constant () {
     
   return wheel * 3600 ;
 }
+
